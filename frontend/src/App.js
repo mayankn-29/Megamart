e1d99919a1c009c5e8cc43686581cd1103a035c2
Prakharpatel-24 <prakharpatel2003@gmail.com>
2022-09-19 00:34:00 +0530

    1st commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/">MegaMart</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
